b9fcf255bcbeda8cba12d4629c02cd214225a448
Vanessa Kleisch <va.kleisch@campus.lmu.de>
2026-03-11 01:14:36 +0100

    Neue partielle effekte

--- analyse/modell_analyse.R
+++ analyse/modell_analyse.R
@@ -20,21 +20,19 @@ options(scipen = 999) # Verhindert wissenschaftliche Notation in ggplot2
 
 # Modelle laden
 model_gam_zentral <- readRDS("modelle/gam_model_zentral.rds") 
-model_gam_ausserhalb_a <- readRDS("modelle/gam_model_ausserhalb_a.rds")
+
 model_gam_ausserhalb_b <- readRDS("modelle/gam_model_ausserhalb_b.rds")
 
 # Modelloutput ansehen
 summary(model_gam_zentral)
-summary(model_gam_ausserhalb_a)
 summary(model_gam_ausserhalb_b)
 
 # Modellgüte evaluieren
 evaluate_confusion_matrix(model_gam_zentral, 
                           test_data = model_data_complete_zentral,
                           y_col = "wohnlage_ebene")
-evaluate_confusion_matrix(model_gam_ausserhalb_a, 
-                          test_data = model_data_complete_ausserhalb,
-                          y_col = "wohnlage_ebene")
+
+                      
 evaluate_confusion_matrix(model_gam_ausserhalb_b, 
                           test_data = model_data_complete_ausserhalb,
                           y_col = "wohnlage_ebene")
@@ -43,9 +41,7 @@ evaluate_confusion_matrix(model_gam_ausserhalb_b,
 evaluate_confusion_matrix_equal_priors(model_gam_zentral, 
                                        test_data = model_data_complete_zentral,
                                        y_col = "wohnlage_ebene")
-evaluate_confusion_matrix_equal_priors(model_gam_ausserhalb_a, 
-                                       test_data = model_data_complete_ausserhalb,
-                                       y_col = "wohnlage_ebene")
+
 evaluate_confusion_matrix_equal_priors(model_gam_ausserhalb_b, 
                                        test_data = model_data_complete_ausserhalb,
                                        y_col = "wohnlage_ebene")
@@ -55,5 +51,4 @@ evaluate_confusion_matrix_equal_priors(model_gam_ausserhalb_b,
 # wenn alle anderen konstant gehalten werden?
 # y-Achse: log odds gegenüber referenzkategorie (durchschnittliche Lage)
 visualize_part_effects(model_gam_zentral, "part_eff", subfolder_name = "part_effects_zent")
-visualize_part_effects(model_gam_ausserhalb_a, "part_eff", subfolder_name = "part_effects_aus_a")
 visualize_part_effects(model_gam_ausserhalb_b, "part_eff", subfolder_name = "part_effects_aus_b")
